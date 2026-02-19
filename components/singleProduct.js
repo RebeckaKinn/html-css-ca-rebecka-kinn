94dfbd86b79b653f21436caa2b3714c4697d7f09
Rebecka Kinn <rebeckaki@getacademy.no>
2026-02-19 14:05:23 +0100

    reusing single product page with js
    Adding product information in an array and filtering according to id to display on the html. Kept the original html, but reusing the page setup with js instead.
    Added a back-to-top button also that I am going to add on the different pages.

---
+++ components/singleProduct.js
@@ -0,0 +1,167 @@
+function fetchSingleProductData(id){
+    const product = products.filter(element => element.id == id);
+    return product[0];
+}
+
+function displaySingleProduct(id){
+    const product = fetchSingleProductData(id);
+    console.log(product || "error getting product")
+    document.getElementById('productDisplay').innerHTML = /*HTML*/`
+    <section class="side-margin flex-colum product-container">
+          <div class="img-container">
+            <img
+              src=${product.image.src}
+              alt=${product.image.alt}
+            />
+          </div>
+
+          <fieldset class="slideshowButtons centered flex-row">
+            <input class="remove-input" type="radio" checked="checked" name="img-slider-1" id="img-slider-1-1" />
+            <label for="img-slider-1-1" class="color-circle bottom-shadow gray-color"></label>
+
+            <input class="remove-input" type="radio" name="img-slider-1" id="img-slider-1-2" />
+            <label for="img-slider-1-2" class="color-circle bottom-shadow gray-color"></label>
+
+            <input class="remove-input" type="radio" name="img-slider-1" id="img-slider-1-3" />
+            <label for="img-slider-1-3" class="color-circle bottom-shadow gray-color"></label>
+
+            <input class="remove-input" type="radio" name="img-slider-1" id="img-slider-1-4" />
+            <label for="img-slider-1-4" class="color-circle bottom-shadow gray-color"></label>
+
+            <input class="remove-input" type="radio" name="img-slider-1" id="img-slider-1-5" />
+            <label for="img-slider-1-5" class="color-circle bottom-shadow gray-color"></label>
+          </fieldset>
+
+          <div class="productInfo flex-colum">
+            <i class="gray-font-s hidden-on-desktop">${product.collection}</i>
+            <div class="flex-row space-between">
+              <h1 class="title remove-margin">${product.title}</h1>
+              <h2 class="title remove-margin hidden-on-desktop">${product.price},-</h2>
+            </div>
+            <i class="gray-font-s hidden-on-mobile">${product.collection}</i>
+
+            <p>${product.itemDescription}</p>
+
+            <a href="#description-section">
+              <h3>➤ Read More</h3>
+            </a>
+
+              <fieldset class="flex-row">
+                <legend class="medium top padding-top-bottom-small">Select Color</legend>
+                ${product.colors.map((element, index) => /*HTML*/`
+                    <input class="remove-input color-chooser-input" type="radio" checked="checked" name="color-picker" id="color-picker-${index}" />
+                    <label class="color-circle product-page-color ${element}-color" for="color-picker-${index}"></label>
+                `).join("")}
+              </fieldset>
+              <fieldset class="flex-row">
+                <legend class="medium padding-top-bottom-small">Select Size</legend>
+                ${product.sizes.map((element, index) => /*HTML*/`
+                    <input class="remove-input size-chooser-input" type="radio" checked="checked" name="size-picker" id="size-picker-${index}" />
+                    <label class="centered color-circle product-page-size gray-color" for="size-picker-${index}">${element}</label>
+                `).join("")}
+                
+              </fieldset>
+
+            <div class="centered space-between-desktop">
+              <input
+                type="checkbox"
+                id="addToCart-toggle"
+              />
+              <label for="addToCart-toggle" class="colored-button font-size" >
+                Add To Cart
+                <img
+                  src="assets/icons/add-shopping-cart.svg"
+                  alt="add-to-card-logo"
+                />
+              </label>
+              <h2 class="title remove-margin hidden-on-mobile">${product.price},-</h2>
+              <section class="addToCart-message bottom-shadow">${addToCartMessage()}</section>
+            </div>
+          </div>
+
+        </section>
+
+        <section class="side-margin flex-colum top-line">
+          <h3 id="description-section">Description</h3>
+          <p>${product.description}</p>
+
+          <h3>Key Features</h3>
+          <ul class="bullet">
+          ${product.keyFeatures.map(element => /*HTML*/`
+            <li>
+                <p>
+                    <b>${element.title}: </b>
+                    ${element.description}
+                </p>
+            </li>
+            `).join("")}
+          </ul>
+        </section>
+
+        <section class="grid-section">
+          <div class="side-margin">
+            <h3>Material & Build</h3>
+            <p>${product.materialAndBuild.description}</p>
+            <ul>
+                ${product.materialAndBuild.information.map(element => /*HTML*/`
+                <li>
+                    <b>${element.title}</b>
+                    <p class="remove-margin bottom-margin">${element.description}</p>
+                </li>
+                `).join("")}
+            </ul>
+          </div>
+
+          <div class="card-img background-img-product-page">${questionAddon()}</div>
+        </section>
+
+        <section class="side-margin flex-colum">
+          <h3>${product.footerText.title}</h3>
+          <p>${product.footerText.description}</p>
+        </section>
+        ${backToTopButton("product-male.html")}
+    `;
+}
+
+function addToCartMessage(){
+    return /*HTML*/`
+            <div class="flex-colum gap">
+                  <div class="flex-row">
+                      <label class="closeButton margin-left-small" for="addToCart-toggle">
+                        <span></span>
+                      </label>
+                      <img class="top-margin" src="assets/icons/added-to-cart.svg" alt="Added to cart icon">
+                        <div class="top-margin">
+                          <h3 class="remove-margin">Item added to your cart</h3>
+                          <p class="remove-margin gray-font-s">2 items total in cart</p>
+
+                        </div>
+                    </div>
+                    
+                    <div class="flex-row align-items-center justify-content-center">
+                      <a class="white-button remove-text-wrap" href="new-arrivals.html">Continue Shopping</a>
+                      <label for="shopping-cart-toggle" class="colored-button remove-text-wrap">Open cart</label>
+                    </div>
+                  </div>
+    `;
+}
+
+function questionAddon(){
+    return /*HTML*/`
+            <div class="col1 coli-flex grid-placement-end">
+              <b class="img-font">Have any questions?</b>
+              <p class="img-font">Contact us for alternatives, sizing or usage. We are happy to
+                help and out customer support is always here to help and guide
+                you.
+              </p>
+              <a
+                href="contact.html"
+                class="white-button square-button bottom-shadow font-size">Contact Us</a>
+            </div>
+            <img
+              class="col2 img-icon img-logo-right"
+              src="assets/images/RainyDays_Logo_inverted.png"
+              alt="RaindyDays Watermark"
+            />
+    `;
+}
---
+++ handeling/data.js
@@ -0,0 +1,211 @@
+const products = [
+    {
+        id: "001",
+        title: "Cecilie Light Wind Anorak",
+        collection: "Cecilie Brand",
+        image: {
+            src: "./assets/images/products/RainyDays_Jacket244.png",
+            alt: "Image of Cecilie Light Wind Anorak - women"
+        },
+        price: 349,
+        type: "women", 
+        category: "exploring",
+        colors: ["grey", "black", "darkRed"],
+        sizes: ["XS", "S", "M", "L", "XL"],
+        cardDescription: [ //on product card
+            "Waterproof up to 20 000 mm.",
+            "Sits comfortable to the body.",
+            "Light and breathable",
+        ],
+
+        itemDescription: "Lightweight and versatile, perfect for hiking, climbing, or everyday wear, this jacket is designed to block out chilly gusts while allowing moisture to escape, keeping you comfortable during high-intensity activities.",
+        description: "Experience ultimate versatility and comfort with the Adventure Pro All-Weather Jacket, designed for outdoor enthusiasts who refuse to let the elements stand in their way. Whether you're hiking through rain-soaked trails, skiing down snowy slopes, or navigating windy city streets, this jacket has got you covered.",
+        keyFeatures: [
+            {
+                title: "Waterproof & Breathable",
+                description: "Made from high-quality, waterproof fabric that keeps you dry while allowing moisture to escape, ensuring you stay comfortable even during intense activities."
+            },
+            {
+                title: "Adjustable Hood",
+                description: "The adjustable hood provides added protection and can be easily stowed away when not needed"
+            },
+            {
+                title: "Multiple Pockets",
+                description: "Conveniently placed pockets provide ample storage for your essentials, including a secure inner pocket for valuables."
+            },
+            {
+                title: "Windproof Design",
+                description: "The windproof construction shields you from harsh winds, keeping you warm and protected."
+            },
+            {
+                title: "Lightweight & Packable",
+                description: "Despite its robust features, the jacket remains lightweight and can be packed down small, making it perfect for travel."
+            },
+            {
+                title: "Reflective Details",
+                description: "Reflective accents enhance visibility in low-light conditions, ensuring safety during nighttime adventures."
+            },
+            {
+                title: "Adjustable Cuffs & Hem",
+                description: "Customize the fit with adjustable cuffs and a drawcord hem to seal out the cold and wet."
+            },
+        ],
+        materialAndBuild: {
+            description: "Made from lightweight yet robust nylon, this jacket is built to withstand the elements without weighing you down.",
+            information: [
+                {
+                    title: "Outer Layer",
+                    description: "100% Polyester with Durable Water Repellent (DWR) finish."
+                },
+                {
+                    title: "Inner Lining",
+                    description: "Breathable mesh for enhanced comfort."
+                },
+                {
+                    title: "Care Instructions",
+                    description: "Machine wash cold with like colors. Tumble dry low. Do not bleach or iron. Follow the care label for detailed instructions."
+                },
+                {
+                    title: "Color Options",
+                    description: "Choose from classic black, vibrant red, or cool navy blue to suit your style."
+                },
+                {
+                    title: "Sizes",
+                    description: "Available in a range of sizes from XS to XL, ensuring a comfortable fit for everyone."
+                },
+            ]
+        },
+        footerText: {
+            title: "Why Choose the Adventure Pro All-Weather Jacket?",
+            description: "Our Adventure Pro All-Weather Jacket is meticulously designed to cater to a variety of outdoor activities, ensuring you stay protected and comfortable no matter the weather. Crafted with premium materials and innovative features, this jacket offers unparalleled performance and durability. Whether you're an avid adventurer or simply looking for a reliable everyday jacket, the Adventure Pro is your go-to choice for all-weather protection."
+        },
+
+    },
+    {
+        id: "002",
+        title: "Robot V2 3L Jacket",
+        collection: "Rabot Collection",
+        image: {
+            src: "./assets/images/products/RainyDays_Jacket1transparent.png",
+            alt: "Image of Rabot V2 3L Jacket - male"
+        },
+        price: 598,
+        type: "men", 
+        category: "other", 
+        colors: ["orange", "black"],
+        sizes: ["XS", "S", "M", "L", "XL"],
+        cardDescription: [ //on product card
+            "Waterproof Construction - 15 000 mm.",
+            "Breathable Fabric.",
+            "Adjustable Fit.",
+            "Versatile Design."
+        ],
+
+        itemDescription: "Prepare for whatever Mother Nature throws your way with our versatile and durable RainGuard™ Waterproof Jacket. Crafted with advanced waterproof technology, this jacket keeps you dry and comfortable even during the heaviest downpours.",
+        description: "Experience ultimate versatility and comfort with the Adventure Pro All-Weather Jacket, designed for outdoor enthusiasts who refuse to let the elements stand in their way. Whether you're hiking through rain-soaked trails, skiing down snowy slopes, or navigating windy city streets, this jacket has got you covered.",
+        keyFeatures: [
+            {
+                title: "Waterproof & Breathable",
+                description: "Made from high-quality, waterproof fabric that keeps you dry while allowing moisture to escape, ensuring you stay comfortable even during intense activities."
+            },
+            {
+                title: "Adjustable Hood",
+                description: "The adjustable hood provides added protection and can be easily stowed away when not needed."
+            },
+            {
+                title: "Multiple Pockets",
+                description: "Conveniently placed pockets provide ample storage for your essentials, including a secure inner pocket for valuables."
+            },
+            {
+                title: "Windproof Design",
+                description: "The windproof construction shields you from harsh winds, keeping you warm and protected."
+            },
+            {
+                title: "Lightweight & Packable",
+                description: "Despite its robust features, the jacket remains lightweight and can be packed down small, making it perfect for travel."
+            },
+            {
+                title: "Reflective Details",
+                description: "Reflective accents enhance visibility in low-light conditions, ensuring safety during nighttime adventures."
+            },
+            {
+                title: "Adjustable Cuffs & Hem",
+                description: "Customize the fit with adjustable cuffs and a drawcord hem to seal out the cold and wet."
+            },   
+        ],
+        materialAndBuild: {
+            description: "Made from lightweight yet robust nylon, this jacket is built to withstand the elements without weighing you down.",
+            information: [
+                {
+                    title: "Outer Layer",
+                    description: "100% Polyester with Durable Water Repellent (DWR) finish."
+                },
+                {
+                    title: "Inner Lining",
+                    description: "Breathable mesh for enhanced comfort."
+                },
+                {
+                    title: "Care Instructions",
+                    description: "Machine wash cold with like colors. Tumble dry low. Do not bleach or iron. Follow the care label for detailed instructions."
+                },
+                {
+                    title: "Color Options",
+                    description: "Choose from classic black, vibrant red, or cool navy blue to suit your style."
+                },
+                {
+                    title: "Sizes",
+                    description: "Available in a range of sizes from XS to XL, ensuring a comfortable fit for everyone."
+                },
+            ]
+        },
+        footerText: {
+            title: "Why Choose the Adventure Pro All-Weather Jacket?",
+            description: "Our Adventure Pro All-Weather Jacket is meticulously designed to cater to a variety of outdoor activities, ensuring you stay protected and comfortable no matter the weather. Crafted with premium materials and innovative features, this jacket offers unparalleled performance and durability. Whether you're an avid adventurer or simply looking for a reliable everyday jacket, the Adventure Pro is your go-to choice for all-weather protection."
+        },
+
+    },
+]
+
+/*
+{
+        id: "",
+        title: "",
+        collection: "",
+        image: {
+            src: "",
+            alt: ""
+        },
+        price: 0,
+        type: "", //women / men
+        category: "", //exploring / other
+        colors: ["", ""],
+        sizes: ["", ""],
+        cardDescription: [ //on product card
+            "",
+            "",
+        ],
+
+        itemDescription: "",
+        description: "",
+        keyFeatures: [
+            {
+                title: "",
+                description: ""
+            },
+        ],
+        materialAndBuild: {
+            description: "",
+            information: [
+                {
+                    title: "",
+                    description: ""
+                },
+            ]
+        },
+        footerText: {
+            title: "",
+            description: ""
+        },
+
+    },
+*/
